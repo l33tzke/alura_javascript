6528cc56eb9a034e52592c5890dc73c4b2cc7457
Andre Leitzke <leitzke.andre@gmail.com>
2017-07-05 23:27:23 -0300

    Finished Lesson 2, added IMC (BMI) calculation, as well as weight and height validation.

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,2 +1,29 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
+
+
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = paciente.querySelector(".info-peso");
+var tdAltura = paciente.querySelector(".info-altura");
+var tdIMC = paciente.querySelector(".info-imc");
+
+var peso = tdPeso.textContent;
+var altura = tdAltura.textContent;
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if (peso <= 0 || peso >= 1000){
+	pesoEhValido = false;
+	tdIMC.textContent = "Peso Inválido";
+}
+
+if (altura <= 0 || altura >= 3.00){
+	alturaEhValida = false;
+	tdIMC.textContent = "Altura Inválida";
+}
+
+if (pesoEhValido && alturaEhValida){
+	var imc = peso / (altura * altura);
+	tdIMC.textContent = imc;
+}
